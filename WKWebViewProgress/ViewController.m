1b6fc86a1df61c04e05698c3333d6aa6a69ab887
王天永 <47940062@qq.com>
2018-02-03 10:33:02 +0800

    webview 加载进度条

--- WKWebViewProgress/ViewController.m
+++ WKWebViewProgress/ViewController.m
@@ -7,23 +7,53 @@
 //
 
 #import "ViewController.h"
+#import <WebKit/WebKit.h>
 
-@interface ViewController ()
+#define urlStr  @"http://www.baidu.com"
 
+@interface ViewController ()<WKUIDelegate,WKNavigationDelegate>
+@property (nonatomic,strong) WKWebView *webView;
+@property (nonatomic,strong) UIProgressView *progressView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.webView = [[WKWebView alloc]initWithFrame:self.view.bounds];
+    self.webView.UIDelegate = self;
+    self.webView.navigationDelegate = self;
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlStr]]];
+    [self.view addSubview:self.webView];
+    
+    self.progressView = [[UIProgressView alloc]initWithFrame:CGRectMake(0, 64, CGRectGetWidth(self.view.frame), 2)];
+    self.progressView.progressTintColor = [UIColor greenColor];
+    [self.view addSubview:self.progressView];
+    
+    // 给webview添加监听
+    [self.webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionOld | NSKeyValueObservingOptionNew context:nil];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+// 监听事件处理
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context{
+    if ([keyPath isEqual:@"estimatedProgress"] && object == self.webView) {
+        [self.progressView setAlpha:1.0f];
+        [self.progressView setProgress:self.webView.estimatedProgress animated:YES];
+        if (self.webView.estimatedProgress  >= 1.0f) {
+            [UIView animateWithDuration:0.3 delay:0.3 options:UIViewAnimationOptionCurveEaseOut animations:^{
+                [self.progressView setAlpha:0.0f];
+            } completion:^(BOOL finished) {
+                [self.progressView setProgress:0.0f animated:YES];
+            }];
+        }
+    }else{
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
 }
-
+- (void)dealloc{
+    [self.webView removeObserver:self forKeyPath:@"estimatedProgress"];
+    [self.webView setNavigationDelegate:nil];
+    [self.webView setUIDelegate:nil];
+}
 
 @end
